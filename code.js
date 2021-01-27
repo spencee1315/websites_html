5bbe2c965d0893bfc906d6b50716649294271a09
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-01-26 21:07:14 -0800

    updating javascript file and updating javascript formatting within index file

--- code.js
+++ code.js
@@ -14,33 +14,3 @@ if (hourNow > 18){
 }
 
 document.write('<h3>' + greeting + '</h3>');
-
-// Create variables for the welcome by name
-var greeting = 'Howdy ';
-var message = 'Elliott';
-
-var welcomeMessage = greeting + name + '!';
-
-var el = document.getElementById('greeting');
-el.textContent = welcomeMessage;
-
-// display a confirmation box and output what the user clicked
-var txt;
-var r = confirm("Press a button!");
-if (r == true) {
-  txt = "You pressed OK!";
-} else {
-  txt = "You pressed Cancel!";
-}
-
-// Display a prompt box which ask the user for her/his name, and output a message
-var userName = prompt('Please Enter your name!');
-
-alert('Hello ' + userName);
-
-
-if(userName == 'Elliott') {
-    document.write('Welcome Darth ' + userName)
-} else {
-    document.write('Welcome ' + userName)
-}
